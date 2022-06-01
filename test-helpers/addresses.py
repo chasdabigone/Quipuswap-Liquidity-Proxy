725ddcbf58fd9d901f999391488288ed5806236e
chas s <chasdabigone@gmail.com>
2022-05-31 23:50:49 -0500

    Create addresses.py

---
+++ test-helpers/addresses.py
@@ -0,0 +1,18 @@
+import smartpy as sp
+
+# This file contains addresses for tests which are named and ensure uniqueness across the test suite.
+
+# The address which acts as the Governor
+GOVERNOR_ADDRESS = sp.address("tz1YYnf7vGXqCmB1shNg4rHiTz5gwWTDYceB")
+
+# The address which acts as the addLiquidity Executor
+EXECUTOR_ADDRESS = sp.address("tz1YYnf7vGXqCmB1shNg4rHiTz5gwWTDYceB")
+
+# The address that acts as the token contract.
+TOKEN_ADDRESS = sp.address("KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV")
+
+# The address of a XTZ/kUSD Quipuswap contract
+QUIPUSWAP_ADDRESS = sp.address("KT1K4EwTpbvYN9agJdjpyJm4ZZdhpUNKB3F6")
+
+# The address of the Harbinger Normalizer (views)
+HARBINGER_ADDRESS = sp.address("KT1ENe4jbDE1QVG1euryp23GsAeWuEwJutQX")
